99ffe0db087b8eb251a4010e5633e2c597b483c8
sweetiea <sweetie.anang@gmail.com>
2016-04-14 13:55:17 +0100

    Initial Commit

--- app/src/main/java/com/example/sweetiean/stlfieldinstallation1/MainActivity.java
+++ app/src/main/java/com/example/sweetiean/stlfieldinstallation1/MainActivity.java
@@ -1259,7 +1259,7 @@ public class MainActivity extends ActionBarActivity {
 
         // Read in the bytes
         int offset = 0;
-        int numRead = 0;
+        int numRead;
         while (offset < bytes.length
                 && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
             offset += numRead;
